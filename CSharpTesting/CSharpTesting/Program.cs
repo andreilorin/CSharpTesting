5e0d71070ea98d7ddd42c6c0822ed207911dbba1
andrei lorin <andreilorin@gmail.com>
2017-10-14 17:56:11 +0100

    Selenium added to the project

--- CSharpTesting/CSharpTesting/Program.cs
+++ CSharpTesting/CSharpTesting/Program.cs
@@ -1,9 +1,21 @@
-﻿namespace CSharpTesting
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace CSharpTesting
 {
     class Program
     {
         static void Main(string[] args)
         {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("http://andreilorin.com");
+
+            IWebElement contactButton = driver.FindElement(By.CssSelector("#contact"));
+
+            contactButton.Click();
+
+            driver.Quit();
         }
     }
 }
