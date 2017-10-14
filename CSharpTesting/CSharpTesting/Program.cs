6a6dee2c5937f6221de1ab00544513a6308bbb90
andrei lorin <andreilorin@gmail.com>
2017-10-14 18:01:37 +0100

    Added NLog for logging

--- CSharpTesting/CSharpTesting/Program.cs
+++ CSharpTesting/CSharpTesting/Program.cs
@@ -1,18 +1,23 @@
-﻿using OpenQA.Selenium;
+﻿using NLog;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
 namespace CSharpTesting
 {
     class Program
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        
         static void Main(string[] args)
         {
             IWebDriver driver = new ChromeDriver();
 
+            logger.Info("Navigating to the website");
             driver.Navigate().GoToUrl("http://andreilorin.com");
 
             IWebElement contactButton = driver.FindElement(By.CssSelector("#contact"));
 
+            logger.Info("Clicking Contact Button");
             contactButton.Click();
 
             driver.Quit();
