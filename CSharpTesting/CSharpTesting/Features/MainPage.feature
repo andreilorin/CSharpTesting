10b55926893dfed90fb76c34f06fb0b66cb3792c
andrei lorin <andreilorin@gmail.com>
2017-10-15 23:06:28 +0100

    added SpecFlow and feature file

---
+++ CSharpTesting/CSharpTesting/Features/MainPage.feature
@@ -0,0 +1,7 @@
+﻿Feature: MainPage
+
+@mytag
+Scenario: Navigate to the main page
+	Given Main Page is accesed
+	When Contact button is pressed
+	Then Contact section is displayed
---
+++ CSharpTesting/CSharpTesting/Features/MainPage.feature.cs
@@ -0,0 +1,1 @@
+﻿#error Generation error: Method 'Initialize' in type 'Baseclass.Contrib.SpecFlow.Selenium.NUnit.GeneratorPlugin' from assembly 'Baseclass.Contrib.SpecFlow.Selenium.NUnit.SpecFlowPlugin, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null' does not have an implementation.
