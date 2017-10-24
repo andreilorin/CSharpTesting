afdeebfe027d646a5a115660694c0983480b83a8
andrei lorin <andreilorin@gmail.com>
2017-10-24 21:25:01 +0100

    added Setup class

---
+++ CSharpTesting/CSharpTesting/StepDefinitions/Setup.cs
@@ -0,0 +1,22 @@
+﻿using Baseclass.Contrib.SpecFlow.Selenium.NUnit.Bindings;
+using OpenQA.Selenium;
+using System.Configuration;
+
+namespace CSharpTesting.StepDefinitions
+{
+    public class Setup
+    {
+        public IWebDriver driver = null;
+
+        public void SetUp()
+        {
+            Browser.Current.Navigate().GoToUrl(ConfigurationManager.AppSettings["seleniumBaseUrl"]);
+            driver = Browser.Current;
+        }
+
+        public void CleanUp()
+        {
+            driver.Close();
+        }
+    }
+}
